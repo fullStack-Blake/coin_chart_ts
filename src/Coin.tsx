3c7a6b3cc34e7609b47793161cb3b2c852f759a5
Blake Han <han.sangyeup@gmail.com>
2021-11-15 17:52:07 -0500

    API call to get info and price

--- src/Coin.tsx
+++ src/Coin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
 
@@ -16,10 +16,29 @@ interface RouteState {
   name: string;
 }
 const Coin = () => {
+  const [loading, setLoading] = useState(true);
   const { coinID } = useParams<RouteParams>();
+  const [info, setInfo] = useState({});
+  const [price, setPrice] = useState({});
   const {
     state: { name },
   } = useLocation<RouteState>();
+
+  useEffect(() => {
+    (async () => {
+      const response = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinID}`)
+      ).json();
+      setInfo(response);
+    })();
+    (async () => {
+      const response = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinID}`)
+      ).json();
+      setPrice(response);
+      console.log(response);
+    })();
+  }, []);
 
   return (
     <Container>
